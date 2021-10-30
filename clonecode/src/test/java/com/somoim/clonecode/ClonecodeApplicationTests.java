660e246fa66f255356f417621d46fc97592cb98a
sosojeong2 <sj20778641@gmail.com>
2021-10-30 14:07:31 +0900

    기본

---
+++ clonecode/src/test/java/com/somoim/clonecode/ClonecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somoim.clonecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
