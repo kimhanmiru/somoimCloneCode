e5e715d52772c6a6df03c138a944b0ae025b6774
sosojeong2 <sj20778641@gmail.com>
2021-11-14 17:53:19 +0900

    hikariCP 설정 변경

--- clonecode/src/main/java/com/somoim/clonecode/config/DatabaseConfig.java
+++ clonecode/src/main/java/com/somoim/clonecode/config/DatabaseConfig.java
@@ -1,47 +1,54 @@
 package com.somoim.clonecode.config;
 
 
-import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.context.annotation.PropertySource;
 
-import javax.sql.DataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
-@MapperScan(basePackages = {"com.somoim.clonecode.mapper"})
-@EnableTransactionManagement
+@PropertySource("classpath:/application.yml")
 public class DatabaseConfig {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
     @Value("${mybatis.mapper-locations}")
     private String mapperLocation;
 
-    @Bean(name = "hpDataSource", destroyMethod = "close")
-    @ConfigurationProperties(prefix = "dbenv.hp.datasource")
-    public DataSource hpDataSource() {
-        return DataSourceBuilder.create().type(HikariDataSource.class).build();
-    }
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource.hikari")
+	public HikariConfig hikariConfig() {
+		return new HikariConfig();
+	}
 
-    @Bean(name = "hpSqlSessionFactory")
-    public SqlSessionFactory hpSqlSessionFactory(@Qualifier("hpDataSource") DataSource hpDataSource,
-                                                       ApplicationContext applicationContext) throws Exception {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(hpDataSource);
-        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources(mapperLocation));
-        return sqlSessionFactoryBean.getObject();
-    }
+	@Bean
+	public DataSource dataSource() {
+		return new HikariDataSource(hikariConfig());
+	}
 
-    @Bean(name = "hpSqlSessionTemplate")
-    public SqlSessionTemplate hpSqlSessionTemplate(SqlSessionFactory hpSqlSessionFactory) throws Exception {
-        return new SqlSessionTemplate(hpSqlSessionFactory);
-    }
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+		factoryBean.setDataSource(dataSource());
+		factoryBean.setMapperLocations(applicationContext.getResources(mapperLocation));
+		return factoryBean.getObject();
+	}
+
+	@Bean
+	public SqlSessionTemplate sqlSession() throws Exception {
+		return new SqlSessionTemplate(sqlSessionFactory());
+	}
 
 }
